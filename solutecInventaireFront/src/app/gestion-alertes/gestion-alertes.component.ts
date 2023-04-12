1ded771f992b43ec73bdb127431fd6bdb004bf08
BastienLeDev <gauthiero.bastien@outlook.com>
2023-04-12 11:29:44 +0200

    Modifs page Alerte

--- solutecInventaireFront/src/app/gestion-alertes/gestion-alertes.component.ts
+++ solutecInventaireFront/src/app/gestion-alertes/gestion-alertes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { HtmlParser } from '@angular/compiler';
+import { MatFormFieldControl } from '@angular/material/form-field';
+
 
 
 
@@ -12,7 +13,8 @@ export interface ProductElement {
   Active: boolean;
   Triggered: boolean;
   Email: boolean;
-  description: Object[];
+  description: String[];
+  isEdit: boolean;
 }
 @Component({
   selector: 'app-gestion-alertes',
@@ -25,6 +27,9 @@ export interface ProductElement {
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
+  providers: [
+    { provide: MatFormFieldControl, useExisting: GestionAlertesComponent }
+  ]
 })
 
 
@@ -32,7 +37,7 @@ export class GestionAlertesComponent implements OnInit {
   listAlert: any;
   dataSource: ProductElement[];
   columnsToDisplay = ['Alerte', "Seuil", 'Active', 'Triggered', 'Email'];
-  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand', 'supprimer'];
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand', 'isEdit', 'supprimer'];
   expandedElement: ProductElement | null;
   listDataSource: Array<any> = [];
   delete: any;
@@ -43,12 +48,26 @@ export class GestionAlertesComponent implements OnInit {
     this.getAlert();
   }
 
+  addRow() {
+    const newRow: ProductElement = {
+      Id: null,
+      Alerte: "",
+      Seuil: 0,
+      Active: false,
+      Triggered: false,
+      Email: false,
+      isEdit: true,
+      description: [""],
+    };
+    this.dataSource = [newRow, ...this.dataSource];
+  }
+
+
   getAlert() {
     this.http.get('http://localhost:8301/getAlert').subscribe({
       next: (data) => {
         this.listAlert = data;
-        console.log(this.listAlert)
-        this.listAlert.map((element: { idAlert: number; nameAlert: string; level: number; state: boolean; notifEmail: boolean; triggered: boolean; products: Object[] }) => {
+        this.listAlert.map((element: { idAlert: number; nameAlert: string; level: number; state: boolean; notifEmail: boolean; triggered: boolean; products: String[] }) => {
           let Tree = {} as ProductElement;
           Tree.Id = element.idAlert;
           Tree.Alerte = element.nameAlert;
@@ -56,11 +75,11 @@ export class GestionAlertesComponent implements OnInit {
           Tree.Active = element.state;
           Tree.Email = element.notifEmail;
           Tree.Triggered = element.triggered;
+          Tree.isEdit = false;
           Tree.description = element.products;
           this.listDataSource.push(Tree);
         })
         this.dataSource = this.listDataSource;
-        console.log(this.dataSource)
       },
       error: (err) => { console.log(err); }
     });
