05a7377ea2df6a8a26a1791d353ec59bf8ac6ff7
BastienLeDev <gauthiero.bastien@outlook.com>
2023-05-05 16:57:18 +0200

    Ajout notifs

--- solutecInventaireFront/src/app/app.component.ts
+++ solutecInventaireFront/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NotifService } from './notif.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  notification: any;
+  constructor(private NotifService: NotifService, private route: Router) { }
+
+  ngOnInit(): void {
+    this.NotifService.getNotifs().subscribe(data => {
+      this.notification = data;
+    })
+    console.log(this.notification)
+  }
+
+  goToAlerte() {
+    this.route.navigateByUrl('alertes');
+  }
+
   title = 'solutecInventaireFront';
 }
