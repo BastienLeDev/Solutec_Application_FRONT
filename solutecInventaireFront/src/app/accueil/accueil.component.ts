34024833b2b8a85787d6c8b67538303989a71c75
LucyDdt <dodetlucy.pro@hotmail.com>
2023-05-23 16:48:57 +0200

    correction bug page accueil

--- solutecInventaireFront/src/app/accueil/accueil.component.ts
+++ solutecInventaireFront/src/app/accueil/accueil.component.ts
@@ -60,6 +60,7 @@ export class AccueilComponent implements OnInit {
   constructor(private http: HttpClient, private route: Router, private redirectService: RedirectionService) { }
 
   ngOnInit(): void {
+    this.ProductsReserved= [];
     this.refreshAlert()
     this.getReservation();
     this.getListStock();
